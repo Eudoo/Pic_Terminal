d1c506741206ae6038fa3800d2faf777180ab06c
Eudes CODO <eudescodo00@gmail.com>
2024-10-19 03:55:10 +0100

    add mainClass and galerie code

--- src/gr12/Galerie.java
+++ src/gr12/Galerie.java
@@ -1,5 +1,37 @@
 package gr12;
 
+import java.util.ArrayList;
+
 public class Galerie {
+
+	private int id_galerie;
+	private String nom_galerie;
+	private Utilisateur usergaleris;
+	private ArrayList<Image> images;
+	
+	// Constructeur 
+	public Galerie(String nom_galerie, Utilisateur usergaleris) {
+		this.nom_galerie = nom_galerie;
+		this.usergaleris = usergaleris;
+		this.images = new ArrayList<>();
+	}
+	
+	// Methodes
+	public void ajouter_image(Image image) {
+		images.add(image);
+		System.out.println("Image " + image.get_titre() + " ajoutée a la galerie " + nom_galerie );
+	}
+	
+	public void supprimer_image(Image image) {
+		images.remove(image);
+		System.out.println("Image " + image.get_titre() + " supprimer de la galerie " + nom_galerie );
+	}
+	
+	public void afficher_galerie() {
+		System.out.println("Galerie : " + nom_galerie);
+		for (Image img : images) {
+			img.afficher_propriete();
+		}
+	}
 
 }
---
+++ src/gr12/MainClass.java
@@ -0,0 +1,10 @@
+package gr12;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
